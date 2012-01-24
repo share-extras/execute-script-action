3ba2cbb8c1b426c89384f226331f7f85b4d052ed
Will Abson <will.abson@gmail.com>
2012-01-24 18:31:21 +0000

    Execute Script Action changes for 4.0 compatibility
    git-svn-id: https://share-extras.googlecode.com/svn/trunk/Execute Script Action@902 a3f5c567-fd0f-3a89-9b71-a290c5a5f590

--- source/web/extras/components/documentlibrary/execute-script-action.js
+++ source/web/extras/components/documentlibrary/execute-script-action.js
@@ -24,61 +24,65 @@
     * @method onActionExecuteScript
     * @param asset {object} Object literal representing one or more file(s) or folder(s) to be actioned
     */
-   Alfresco.doclib.Actions.prototype.onActionExecuteScript = function DL_onActionExecuteScript(asset)
+   YAHOO.Bubbling.fire("registerAction",
    {
-      var nodeRef = asset.nodeRef,
-         displayName = asset.displayName,
-         actionUrl = Alfresco.constants.PROXY_URI + $combine("slingshot/doclib/action/execute-script/node", nodeRef.replace(":/", ""));
-      
-      // Always create a new instance
-      this.modules.executeScript = new Alfresco.module.SimpleDialog(this.id + "-executeScript").setOptions(
+      actionName: "onActionExecuteScript",
+      fn: function DL_onActionGeotag(asset)
       {
-         width: "30em",
-         templateUrl: Alfresco.constants.URL_SERVICECONTEXT + "extras/modules/documentlibrary/execute-script",
-         actionUrl: actionUrl,
-         firstFocus: this.id + "-executeScript-script",
-         onSuccess:
-         {
-            fn: function dlA_onActionExecuteScript_success(response)
-            {
-               // TODO Persist script to prefs service
-               YAHOO.Bubbling.fire("metadataRefresh",
-               {
-                  highlightFile: displayName
-               });
-               Alfresco.util.PopupManager.displayMessage(
-               {
-                  text: this.msg("message.execute-script.success", displayName)
-               });
-            },
-            scope: this
-         },
-         onFailure:
-         {
-            fn: function dlA_onActionExecuteScript_failure(response)
-            {
-               Alfresco.util.PopupManager.displayMessage(
-               {
-                  text: this.msg("message.execute-script.failure", displayName)
-               });
-            },
-            scope: this
-         },
-         doSetupFormsValidation:
-         {
-            fn: function dlA_onActionExecuteScript_doSetupFormsValidation(p_form)
-            {
-               // Validation
-               p_form.addValidation(this.id + "-executeScript-script", function fnValidateType(field, args, event, form, silent, message)
-               {
-                  return field.options[field.selectedIndex].value !== "-";
-               }, null, "change");
-               p_form.setShowSubmitStateDynamically(true, false);
-               // TODO Retrieve last-actioned script from prefs service
-            },
-            scope: this
-         }
-      });
-      this.modules.executeScript.show();
-   };
+          var nodeRef = asset.nodeRef,
+             displayName = asset.displayName,
+             actionUrl = Alfresco.constants.PROXY_URI + $combine("slingshot/doclib/action/execute-script/node", nodeRef.replace(":/", ""));
+          
+          // Always create a new instance
+          this.modules.executeScript = new Alfresco.module.SimpleDialog(this.id + "-executeScript").setOptions(
+          {
+             width: "30em",
+             templateUrl: Alfresco.constants.URL_SERVICECONTEXT + "extras/modules/documentlibrary/execute-script",
+             actionUrl: actionUrl,
+             firstFocus: this.id + "-executeScript-script",
+             onSuccess:
+             {
+                fn: function dlA_onActionExecuteScript_success(response)
+                {
+                   // TODO Persist script to prefs service
+                   YAHOO.Bubbling.fire("metadataRefresh",
+                   {
+                      highlightFile: displayName
+                   });
+                   Alfresco.util.PopupManager.displayMessage(
+                   {
+                      text: this.msg("message.execute-script.success", displayName)
+                   });
+                },
+                scope: this
+             },
+             onFailure:
+             {
+                fn: function dlA_onActionExecuteScript_failure(response)
+                {
+                   Alfresco.util.PopupManager.displayMessage(
+                   {
+                      text: this.msg("message.execute-script.failure", displayName)
+                   });
+                },
+                scope: this
+             },
+             doSetupFormsValidation:
+             {
+                fn: function dlA_onActionExecuteScript_doSetupFormsValidation(p_form)
+                {
+                   // Validation
+                   p_form.addValidation(this.id + "-executeScript-script", function fnValidateType(field, args, event, form, silent, message)
+                   {
+                      return field.options[field.selectedIndex].value !== "-";
+                   }, null, "change");
+                   p_form.setShowSubmitStateDynamically(true, false);
+                   // TODO Retrieve last-actioned script from prefs service
+                },
+                scope: this
+             }
+          });
+          this.modules.executeScript.show();
+      }
+   });
 })();
